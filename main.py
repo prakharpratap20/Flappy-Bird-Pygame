46500be296fe7651a22dc0007f1086162ec1ad4c
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-06-06 19:44:38 +0530

    created background

---
+++ main.py
@@ -0,0 +1,53 @@
+import pygame
+import sys
+import time
+from settings import WINDOW_HEIGHT, WINDOW_WIDTH, FRAMERATE
+from sprites import BG
+
+
+class Game:
+
+    def __init__(self):
+        # setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode(
+            (WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Flappy Bird")
+        self.clock = pygame.time.Clock()
+
+        # sprite groups
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+
+        # scalte factor
+        bg_height = pygame.image.load(
+            "./graphics/environment/background.png").get_height()
+        self.scale_factor = WINDOW_HEIGHT / bg_height
+
+        # sprite setup
+        BG(self.all_sprites, self.scale_factor)
+
+    def run(self):
+        last_time = time.time()
+        while True:
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # game logic
+            self.all_sprites.update(dt)
+            self.all_sprites.draw(self.display_surface)
+
+            pygame.display.update()
+            self.clock.tick(FRAMERATE)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
